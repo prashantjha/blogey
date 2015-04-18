95db055142cb1846798234a9df2132d6337d8c7e
prashantjha <p4prashant21@gmail.com>
2015-04-18 09:38:53 +0530

    heroku setup

--- blogey/settings.py
+++ blogey/settings.py
@@ -9,9 +9,15 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 import os
 PROJECT_DIR = os.getcwd()
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 
 
 # Quick-start development settings - unsuitable for production
